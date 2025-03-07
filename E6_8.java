24ecf47537a6cf5d563213f8d7445bd449bd679a
AsreJadid <y.saeidian2005@gmail.com>
2025-03-07 23:41:43 +0330

    exercise E6.8

---
+++ E6_8.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class E6_8 {
+    public static void main(String[] args) {
+        System.out.println("enter a name:");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        scan.close();
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
+    }
+}
