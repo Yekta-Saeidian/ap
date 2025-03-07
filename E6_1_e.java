79fd93d533fc36660016b5bf38cbdf4b9fc3fe4b
AsreJadid <y.saeidian2005@gmail.com>
2025-03-07 13:12:24 +0330

    exercise E6.1_e

---
+++ E6_1_e.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class E6_1_e {
+    public static void main(String[] args) {
+        System.out.println("enter a number:");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        int residual = 0;
+        int sum = 0;
+        while (number > 0) {
+            residual = number % 10;
+            if (residual % 2 != 0)
+                sum += residual;
+            number /= 10;
+        }
+        System.out.println(sum);
+    }
+}
