ee92f3b8794a905483e2730b96c59fd897e22207
AsreJadid <y.saeidian2005@gmail.com>
2025-03-24 20:11:37 +0330

    EX2_PM_1_1

--- exercises/EX2_PM_1_1.java
+++ exercises/EX2_PM_1_1.java
@@ -1,4 +1,4 @@
-package ex2;
+package exercises;
 
 import java.util.Scanner;
 
@@ -8,18 +8,18 @@ public class EX2_PM_1_1 {
         Scanner scan = new Scanner(System.in);
         int k = scan.nextInt();
         scan.close();
-        for (int i = 0; i < k+2; i++) {
+        for (int i = 0; i < k + 2; i++) {
             System.out.print("*");
         }
         System.out.println();
         for (int i = 0; i < k; i++) {
             System.out.print("*");
-            for (int j = 0; j < k-2; j++) {
+            for (int j = 0; j < k - 2; j++) {
                 System.out.print(" ");
             }
             System.out.println("*");
         }
-        for (int i = 0; i < k+2; i++) {
+        for (int i = 0; i < k + 2; i++) {
             System.out.print("*");
         }
     }
