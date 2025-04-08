1524d0fccfebc04c56876ec4146819f2a2607ffe
AsreJadid <y.saeidian2005@gmail.com>
2025-04-08 21:47:12 +0330

    EX3_LM_1_1

--- exercises/ex3/EX3_LM_1_1.java
+++ exercises/ex3/EX3_LM_1_1.java
@@ -7,19 +7,36 @@ public class EX3_LM_1_1 {
         Book book1 = new Book("Fundamentals of Physics", "Martha Smith", 1200, 2029);
         Book book2 = new Book("Thomas' Calculus", "George B. Thomas Jr.", 3000, 2018);
 
+        Student student1 = new Student("Dina" , "Saeidian" , 403463124 , "computer science");
+        Student student2 = new Student("Neda" , "Rashtchi" , 401463120 , "physics");
+
         System.out.println("book information:");
         System.out.println();
         book1.showBookInformation();
         book2.showBookInformation();
 
+        System.out.println("student information:");
+        System.out.println();
+        student1.showStudentInformation();
+        student2.showStudentInformation();
+
         book1.setAuthor("David Halliday");
         book2.setYear(2014);
         book2.setPages(1200);
+
+        student1.setFirstName("Yekta");
+        student2.setID(403463121);
+        student2.setField("computer science");
 
         System.out.println("edited book information:");
         System.out.println();
         book1.showBookInformation();
         book2.showBookInformation();
+
+        System.out.println("edited student information:");
+        System.out.println();
+        student1.showStudentInformation();
+        student2.showStudentInformation();
     }
 }
 
@@ -57,6 +74,44 @@ class Book {
         System.out.println("Author: " + author);
         System.out.println("Pages: " + pages);
         System.out.println("Year: " + year);
+        System.out.println("-------------------------");
+    }
+}
+
+class Student {
+   private String firstName;
+   private String lastName;
+   private int ID;
+   private String field;
+
+    public Student (String firstName, String lastName, int ID, String field) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ID = ID;
+        this.field = field;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public void showStudentInformation() {
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("ID: " + ID);
+        System.out.println("Field: " + field);
         System.out.println("-------------------------");
     }
 }
