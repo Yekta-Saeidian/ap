eba43af93a2ecd6093ecb502fad9ba74846af6b4
AsreJadid <y.saeidian2005@gmail.com>
2025-04-08 19:16:01 +0330

    EX2_PM_2_4

--- exercises/ex2/PacmanEngine.java
+++ exercises/ex2/PacmanEngine.java
@@ -20,7 +20,7 @@ public class PacmanEngine {
     }
 
     private void initializeGame() {
-        place = new char[k+2][k+2];
+        place = new char[k + 2][k + 2];
 
         for (int i = 0; i < k + 2; i++) {
             for (int j = 0; j < k + 2; j++) {
@@ -51,5 +51,76 @@ public class PacmanEngine {
         score = 0;
         startTime = System.currentTimeMillis();
         gameOver = false;
+    }
+
+    public void printMatrix() {
+        for (int i = 0; i < k + 2; i++) {
+            for (int j = 0; j < k + 2; j++) {
+                System.out.print(place[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printScore() {
+        System.out.println("Score: " + score);
+    }
+
+    public void printRemainTime() {
+        if (gameOver) {
+            long totalTime = System.currentTimeMillis() - startTime;
+            System.out.println("Game Over! Total time: " + totalTime + "ms");
+        } else {
+            long totalTime = System.currentTimeMillis() - startTime;
+            System.out.println("Time is up!  " + totalTime + "ms");
+        }
+    }
+
+    public void move(int direction) {
+        if (gameOver)
+            return;
+
+        char newPlace = ' ';
+        int newA = a, newB = b;
+
+        switch (direction) {
+            case 0:
+                newA = a - 1;
+                newB = b;
+                break;
+            case 1:
+                newA = a;
+                newB = b - 1;
+                break;
+            case 2:
+                newA = a + 1;
+                newB = b;
+                break;
+            case 3:
+                newA = a;
+                newB = b + 1;
+                break;
+            default:
+                return;
+        }
+
+        if (place[newA][newB] == '*') {
+            System.out.println("Warning! You hit the wall.");
+            return;
+        }
+
+        place[a][b] = ' ';
+        a = newA;
+        b = newB;
+
+        if (place[a][b] == '.') {
+            score++;
+        }
+
+        place[a][b] = 'X';
+
+        if (score == c) {
+            gameOver = true;
+        }
     }
 }
